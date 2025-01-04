ade5b3be425134aac462e77eb2c5d6b8c32c123c
AyushBhatt1975 <ayushbhatt994@gmail.com>
2025-01-04 12:23:53 +0530

    Update Problem1.java

--- Ayush_Bhatt/PrefixAndSuffixAssessment/Problem1.java
+++ Ayush_Bhatt/PrefixAndSuffixAssessment/Problem1.java
@@ -1,34 +1,36 @@
-//package test DSA;
+package testDSA;
 
 public class Problem1 {
-    public void findMiddleIndex(int []nums){
+
+    public static int findMiddleIndex(int[] nums) {
         int totalSum = 0;
         int leftSum = 0;
-//initialze the total and left sum as zero
-        for(num : nums){
-            totalSum + = nums;//calculate the total sum
-        }
-        for(int i = 0;i<nums.length;i++){
-            int rightSum = totalSum - leftSum - nums[i]; // calculate the right sum
-            if ( leftSum = rightSum )//check if both the rightSum and LeftSum are equal
-            {
-               return i; //return i index
-            }
-            leftSum +=nums[i];
-        }
-        return -1;
 
+        
+        for (int num : nums) {
+            totalSum += num; 
         }
-        public static void main(String[] args) {
-            int[] nums1 = {2, 3, -1, 8, 4};
-            int[] nums2 = {1, -1, 4};
-            int[] nums3 = {2, 5};
-    
-            System.out.println(findMiddleIndex(nums1)); 
-            System.out.println(findMiddleIndex(nums2)); 
-            System.out.println(findMiddleIndex(nums3));
+
+        
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = totalSum - leftSum - nums[i]; 
+            if (leftSum == rightSum) { 
+                return i; 
+            }
+            leftSum += nums[i]; 
         }
+
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {2, 3, -1, 8, 4};
+        int[] nums2 = {1, -1, 4};
+        int[] nums3 = {2, 5};
+
+        System.out.println(findMiddleIndex(nums1)); 
+        System.out.println(findMiddleIndex(nums2)); 
+        System.out.println(findMiddleIndex(nums3)); 
     }
 }
-//time complexity---- O(2n)
-//space complexity ---- O(1)
+
